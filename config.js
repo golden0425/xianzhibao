7698d9b58ac5c4fc20bfc0e0a0f550dd3ed37755
aXi <goldenwang0425@gmail.com>
2019-06-11 22:47:27 +0800

    fix:代码提交

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.config = {
- configUrl: 'http://fff64525.ngrok.io/', //测试环境
- imgUrl: 'http://r2w4905263.qicp.vip:57485/ershou/', //图片前缀
-}
+  configUrl: "http://1eea26c7.ngrok.io/", //测试环境
+  imgUrl: "http://r2w4905263.qicp.vip:57485/" //图片前缀
+};
--- pages/fenlei/fenlei.js
+++ pages/fenlei/fenlei.js
@@ -1,208 +1,368 @@
 Page({
   data: {
     activeIndex: 0,
-    scrolltop:0,
+    scrolltop: 0,
     tabs: [
       {
         id: 1,
-        tabName: '二手专区'
-      }, {
+        tabName: "二手专区"
+      },
+      {
         id: 2,
-        tabName: '热搜推荐'
-      }, {
+        tabName: "热搜推荐"
+      },
+      {
         id: 3,
-        tabName: '精品好物'
-      }, {
+        tabName: "精品好物"
+      },
+      {
         id: 4,
-        tabName: '新品热卖'
-      }, {
+        tabName: "新品热卖"
+      },
+      {
         id: 5,
-        tabName: '即刻秒杀'
+        tabName: "即刻秒杀"
       }
     ],
-    items:[
-      {name: 'dangmian', value: '当面交易'},
-      {name: 'dangmian', value: '邮寄',checked: 'true'},
+    items: [
+      { name: "dangmian", value: "当面交易" },
+      { name: "dangmian", value: "邮寄", checked: "true" }
     ],
     ershouzhuanqu: null,
-    resoutuijian:null,
-    jingpinhaoshu:null,
-    xinshuremai:null,
-    jikemiaosha:null,
-    show:false,//控制下拉列表的显示隐藏，false隐藏、true显示
-    selectData:['请选择分类','1','2','3','4','5','6'],//下拉列表的数据
-    index:0,//选择的下拉列表下标
-    selectList:['请选择分类','全新','9.9成新','9.8成新','9.5成新','9成新'],
-    showList:false,
-    value:0
+    resoutuijian: null,
+    jingpinhaoshu: null,
+    xinshuremai: null,
+    jikemiaosha: null,
+    show: false, //控制下拉列表的显示隐藏，false隐藏、true显示
+    selectData: ["请选择分类", "1", "2", "3", "4", "5", "6"], //下拉列表的数据
+    index: 0, //选择的下拉列表下标
+    selectList: [
+      "请选择分类",
+      "全新",
+      "9.9成新",
+      "9.8成新",
+      "9.5成新",
+      "9成新"
+    ],
+    showList: false,
+    value: 0
   },
-  search_page: function () {
+  search_page: function() {
     wx.navigateTo({
-      url: '../search_page/search_page',
-      success: function (res) { },
-      fail: function (res) { },
-      complete: function (res) { },
-    })
+      url: "../search_page/search_page",
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {}
+    });
   },
-  selectTap(){
+  selectTap() {
     this.setData({
       show: !this.data.show,
-      showList:false
+      showList: false
     });
   },
-  selectListTap(){
-   this.setData({
+  selectListTap() {
+    this.setData({
       showList: !this.data.showList,
-      show:false
+      show: false
     });
   },
-  optionListTap(e){
-    let {value,item}=e.currentTarget.dataset;//获取点击的下拉列表的下标
+  optionListTap(e) {
+    let { value, item } = e.currentTarget.dataset; //获取点击的下拉列表的下标
     this.setData({
-      value:value,
-      showList:!this.data.showList,
+      value: value,
+      showList: !this.data.showList
     });
-  }, 
+  },
   // 点击下拉列表
-  optionTap(e){
-    let Index=e.currentTarget.dataset.index;//获取点击的下拉列表的下标
+  optionTap(e) {
+    let Index = e.currentTarget.dataset.index; //获取点击的下拉列表的下标
     this.setData({
-      index:Index,
-      show:!this.data.show,
+      index: Index,
+      show: !this.data.show
     });
   },
 
-  onLoad: function (options) {
+  //上传图片
+  uploadPic: function() {
+    let that = this;
+    if (that.data.filepath.length === 1) {
+      wx.showModal({
+        title: "提示",
+        content: "图片最多只能放一张哦",
+        showCancel: false, //是否显示取消按钮
+        cancelColor: "skyblue", //取消文字的颜色
+        confirmText: "我知道了", //默认是“确定”
+        confirmColor: "skyblue", //确定文字的颜色
+        success: function(res) {},
+        fail: function(res) {}, //接口调用失败的回调函数
+        complete: function(res) {} //接口调用结束的回调函数（调用成功、失败都会执行）
+      });
+      return 0;
+    }
+    //设置第一张上传时弹出提示框消耗流量
+    if (that.data.filepath.length == 0) {
+      wx.showModal({
+        title: "提示",
+        content: "上传图片需要消耗流量，是否继续？",
+        confirmText: "继续",
+        success: function(res) {
+          if (res.confirm) {
+            wx.chooseImage({
+              count: 1, // 默认9
+              sizeType: ["compressed"], //压缩图
+              sourceType: ["album", "camera"], // 可以指定来源是相册还是相机，默认二者都有
+              success: function(res) {
+                // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+                var filepath = that.data.filepath;
+                var tempFilePaths = res.tempFilePaths;
+                console.log(filepath);
+                filepath.push(tempFilePaths);
+                console.log("地址路径为：" + tempFilePaths);
+                that.setData({
+                  isSrc: true,
+                  filepath: filepath,
+                  imgSrc: wx
+                    .getFileSystemManager()
+                    .readFileSync(res.tempFilePaths[0], "base64") //单张地址base64
+                });
+                console.log("地址路径为：" + that.data.filepath[0]);
+                console.log(
+                  wx
+                    .getFileSystemManager()
+                    .readFileSync(res.tempFilePaths[0], "base64")
+                );
+              }
+            });
+            // wx.chooseImage({
+            //   count: 1, // 默认9
+            //   sizeType: ['compressed'], //压缩图
+            //   sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            //   success: function (res) {
+            //     // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+            //     var filepath = that.data.filepath;
+            //     var tempFilePaths = res.tempFilePaths;
+            //     console.log(filepath);
+            //     filepath.push(tempFilePaths);
+            //     console.log("地址路径为：" + tempFilePaths);
+            //     that.setData({
+            //       isSrc: true,
+            //       filepath: filepath,
+            //       imgSrc:tempFilePaths //单张地址
+            //     })
+            //     console.log("地址路径为：" + that.data.filepath[0]);
+            //   }
+            // })
+          }
+        }
+      });
+    } else {
+      wx.chooseImage({
+        count: 1, // 默认9
+        sizeType: ["compressed"], //压缩图
+        sourceType: ["album", "camera"], // 可以指定来源是相册还是相机，默认二者都有
+        success: function(res) {
+          // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+          var filepath = that.data.filepath;
+          var tempFilePaths = res.tempFilePaths;
+          console.log(filepath);
+          filepath.push(tempFilePaths);
+          console.log("地址路径为：" + tempFilePaths);
+          that.setData({
+            isSrc: true,
+            filepath: filepath
+          });
+          console.log("地址路径为：" + that.data.filepath[0]);
+        }
+      });
+    }
+  },
+  //删除图片
+  clearPic: function(event) {
+    //删除图片
+    console.log(event);
+    var index = event.currentTarget.dataset.index;
+    var filepath = that.data.filepath;
+    filepath.splice(index, 1);
+    that.setData({
+      isSrc: true,
+      filepath: filepath
+      // isSrc用来掩盖增加图标
+    });
+  },
+  onLoad: function(options) {
     // 页面初始化 options为页面跳转所带来的参数
-    var that =this;
+    var that = this;
+    that.setData({
+      //初始化数据
+      filepath: [],
+      isSrc: true,
+      ishide: "0",
+      autoFocus: true,
+      isLoading: false,
+      loading: true
+    });
     wx.getSystemInfo({
-      success: (res) => {
+      success: res => {
         that.setData({
-          deviceHeight: res.windowHeight,
+          deviceHeight: res.windowHeight
         });
       }
-    }); 
+    });
     // 获取热搜推荐数据
     wx.request({
-      url: 'https://www.ffgbookbar.cn/BookStoreProject/public/store.php/showBooks',
-      data: { isAll: 1,'type':1},
-      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      url:
+        "https://www.ffgbookbar.cn/BookStoreProject/public/store.php/showBooks",
+      data: { isAll: 1, type: 1 },
+      method: "GET", // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       header: { "content-type": "application/json" }, // 设置请求的 header
-      success: function (res) {
+      success: function(res) {
         var resoutuijian = res.data.reverse();
-       that.setData({
-         resoutuijian: resoutuijian,
-       });
+        that.setData({
+          resoutuijian: resoutuijian
+        });
       }
     });
     // 获取精品好书数据
     wx.request({
-      url: 'https://www.ffgbookbar.cn/BookStoreProject/public/store.php/showBooks',
-      data: { isAll: 1, 'type': 2 },
-      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      url:
+        "https://www.ffgbookbar.cn/BookStoreProject/public/store.php/showBooks",
+      data: { isAll: 1, type: 2 },
+      method: "GET", // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       header: { "content-type": "application/json" }, // 设置请求的 header
-      success: function (res) {
+      success: function(res) {
         var jingpinhaoshu = res.data.reverse();
         that.setData({
-          jingpinhaoshu: jingpinhaoshu,
+          jingpinhaoshu: jingpinhaoshu
         });
       }
     });
     // 获取新书热卖数据
     wx.request({
-      url: 'https://www.ffgbookbar.cn/BookStoreProject/public/store.php/showBooks',
-      data: { isAll: 1, 'type': 3 },
-      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      url:
+        "https://www.ffgbookbar.cn/BookStoreProject/public/store.php/showBooks",
+      data: { isAll: 1, type: 3 },
+      method: "GET", // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       header: { "content-type": "application/json" }, // 设置请求的 header
-      success: function (res) {
+      success: function(res) {
         var xinshuremai = res.data.reverse();
         that.setData({
-          xinshuremai: xinshuremai,
+          xinshuremai: xinshuremai
         });
       }
     });
     // 获取即刻秒杀数据
     wx.request({
-      url: 'https://www.ffgbookbar.cn/BookStoreProject/public/store.php/showBooks',
-      data: { isAll: 1, 'type': 4 },
-      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      url:
+        "https://www.ffgbookbar.cn/BookStoreProject/public/store.php/showBooks",
+      data: { isAll: 1, type: 4 },
+      method: "GET", // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       header: { "content-type": "application/json" }, // 设置请求的 header
-      success: function (res) {
+      success: function(res) {
         var jikemiaosha = res.data.reverse();
         that.setData({
-          jikemiaosha: jikemiaosha,
+          jikemiaosha: jikemiaosha
         });
       }
     });
     // 获取二手旧书数据
     wx.request({
-      url: 'https://www.ffgbookbar.cn/BookStoreProject/public/store.php/showBooks',
-      data: { isAll: 1, 'type': 5 },
-      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      url:
+        "https://www.ffgbookbar.cn/BookStoreProject/public/store.php/showBooks",
+      data: { isAll: 1, type: 5 },
+      method: "GET", // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
       header: { "content-type": "application/json" }, // 设置请求的 header
-      success: function (res) {
+      success: function(res) {
         var ershouzhuanqu = res.data.reverse();
         that.setData({
-          ershouzhuanqu: ershouzhuanqu,
+          ershouzhuanqu: ershouzhuanqu
         });
       }
     });
   },
-  changeTab: function (e) {
+  changeTab: function(e) {
     this.setData({
       activeIndex: e.currentTarget.dataset.index,
-      scrolltop:0,
-    })
-    switch(e.currentTarget.dataset.index) {
-     case 1:
-       this.setData({
-        selectData:['请选择分类','热搜推荐1','热搜推荐2','热搜推荐3','热搜推荐4','热搜推荐5'] 
-       })
+      scrolltop: 0
+    });
+    switch (e.currentTarget.dataset.index) {
+      case 1:
+        this.setData({
+          selectData: [
+            "请选择分类",
+            "热搜推荐1",
+            "热搜推荐2",
+            "热搜推荐3",
+            "热搜推荐4",
+            "热搜推荐5"
+          ]
+        });
         break;
-     case 2:
-      this.setData({
-        selectData:['请选择分类','精品好物1','精品好物2','精品好物3','精品好物4','精品好物5']
-       })
+      case 2:
+        this.setData({
+          selectData: [
+            "请选择分类",
+            "精品好物1",
+            "精品好物2",
+            "精品好物3",
+            "精品好物4",
+            "精品好物5"
+          ]
+        });
         break;
       case 3:
-       this.setData({
-        selectData:['请选择分类','新品热卖1','新品热卖2','新品热卖3','新品热卖4','新品热卖5']
-       })
-       break;
+        this.setData({
+          selectData: [
+            "请选择分类",
+            "新品热卖1",
+            "新品热卖2",
+            "新品热卖3",
+            "新品热卖4",
+            "新品热卖5"
+          ]
+        });
+        break;
       case 4:
-         this.setData({
-        selectData:['请选择分类','即刻秒杀1','即刻秒杀2','即刻秒杀3','即刻秒杀4','即刻秒杀5']
-       })
-      break;
+        this.setData({
+          selectData: [
+            "请选择分类",
+            "即刻秒杀1",
+            "即刻秒杀2",
+            "即刻秒杀3",
+            "即刻秒杀4",
+            "即刻秒杀5"
+          ]
+        });
+        break;
       default:
-      this.setData({
-        selectData:['请选择分类','1','2','3','4','5','6']
-       })
-} 
+        this.setData({
+          selectData: ["请选择分类", "1", "2", "3", "4", "5", "6"]
+        });
+    }
   },
-  gotodetail:function(res){ 
+  gotodetail: function(res) {
     var bookid = res.currentTarget.dataset.bookid;
     wx.navigateTo({
-      url: '../others/details/details?bookid='+bookid,
-    })
+      url: "../others/details/details?bookid=" + bookid
+    });
   },
-  onShareAppMessage: function (res) {
+  onShareAppMessage: function(res) {
     if (res.from === "menu") {
-      console.log("来自右上角转发菜单")
+      console.log("来自右上角转发菜单");
     }
     return {
       title: "肥肥怪书吧",
-      path: '/pages/index/index',
+      path: "/pages/index/index",
       imageUrl: "https://www.ffgbookbar.cn/BookStoreProject/public/index.png",
-      success: (res) => {
+      success: res => {
         console.log("转发成功", res);
       },
-      fail: (res) => {
+      fail: res => {
         console.log("转发失败", res);
       }
-    }
+    };
   },
-  getMore: function () {
+  getMore: function() {
     // this.setData({
     //   contentList: this.data.contentList.concat([
     //     { text: ' ' },
@@ -212,6 +372,5 @@ Page({
     //     { text: ' ' }
     //   ])
     // });
-  },
-  
-})
+  }
+});
